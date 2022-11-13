d93ff3061b5b9db1050979195eec396d900ff2ae
Alfinfdw <113610587+Alfinfdw@users.noreply.github.com>
2022-11-13 07:44:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,26 +14,26 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['4915256954581'] //ur owner number
+global.ownername = "NiluhChan" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "Indonesia, Jawa timur, Jawa" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213'] //ur number
-global.ownernomer = "916909137213" //ur number
-global.ownertag = '916909137213' //ur tag number
-global.premium = ['916909137213'] //ur premium number
-global.botname = 'Cheems Bot V5' //ur bot name
-global.ownername = "🦄Dream Guy Xeon"
+global.owner = ['4915256954581'] //ur number
+global.ownernomer = "4915256954581" //ur number
+global.ownertag = '4915256954581' //ur tag number
+global.premium = ['4915256954581'] //ur premium number
+global.botname = 'NiluhChanbot' //ur bot name
+global.ownername = "🦄NiluhChan"
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD5' //script link
-global.themeemoji = "🦄" //ur theme emoji
+global.themeemoji = "🦄niluhChan" //ur theme emoji
 global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.author = "NiluhChan" //ur sticker watermark author
+global.wm = "Niluh Chan Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
